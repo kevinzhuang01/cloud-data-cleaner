4033a36cc7df482b9a96b08a45696ac0348554f2
kevinzhuang01 <kevinhong79928@gmail.com>
2025-07-24 13:31:33 -0700

    fix port

--- main.py
+++ main.py
@@ -335,4 +335,4 @@ def delete_file(file_id):
             conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
